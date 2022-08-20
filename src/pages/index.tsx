09cb6ad52716f79b14913b9378e1d6bafed5e401
namooplus <namoo.developer@gmail.com>
2022-08-20 16:34:31 +0900

    Initialization : Add styled-components

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,29 @@
+import styled from "styled-components";
+
 interface HomeProps {}
 
 const Home = ({}: HomeProps) => {
-  return <div>DevBoard FE</div>;
+  return (
+    <Wrapper>
+      <Title>
+        DevBoard <Emphasize>FE</Emphasize>
+      </Title>
+    </Wrapper>
+  );
 };
 
 export default Home;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100vh;
+`;
+
+const Title = styled.h1``;
+
+const Emphasize = styled.span`
+  color: #4eafcf;
+`;
